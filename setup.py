14cbbe72bc470f3bd16c19306d913245ec2c8fa3
Sourabh Maity <maity.sourabh13@gmail.com>
2019-09-05 16:41:52 +0530

    changed project name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="colada",
+    name="jupyda",
     version="0.0.1",
     author="Sourabh Maity",
     author_email="maity.sourabh13@gmail.com",
-    description="IPython magic extension to run CUDA C code on Google colab.",
+    description="IPython magic extension to run CUDA C code on jupyter notebooks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sourabhXIII/colada",
+    url="https://github.com/sourabhXIII/jupyda",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
